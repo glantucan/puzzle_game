401bc9d8343bb2d5e07fabe34cee569a12799661
Glantucan <glantucan@gmail.com>
2016-08-23 14:46:42 +0100

    basic movement with user input

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	[SerializeField] private float moveVel = 10F;
+	private Rigidbody rb;
+
+	void Start () {
+		this.rb = this.GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		float xInput = Input.GetAxis("Horizontal");
+		float zInput = Input.GetAxis("Vertical");
+		Vector3 direction = zInput * Vector3.forward + xInput * Vector3.right;   
+		if (direction != Vector3.zero) {
+			this.rb.velocity = direction.normalized * moveVel;
+		}
+	}
+}
